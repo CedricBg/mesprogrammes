237da005536dcdfe1fa5b5ebaea769bbfbe17895
bogaert cedric <bogaert@outlook.com>
2023-06-10 10:14:42 +0200

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
+import { Routes, provideRouter } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: 'presentation' ,  title:'Présentation', loadComponent: ()=> import('./components/presentation/presentation.component').then(module =>module.PresentationComponent)},
+  { path: 'cv' ,  title:'Cv', loadComponent: ()=> import('./components/cv/cv.component').then(module =>module.CvComponent)},
+]
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [provideRouter(routes)]
 };
+
