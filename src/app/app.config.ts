9df85ce7931950173c5d68e55065c68a6af63e97
bogaert cedric <bogaert@outlook.com>
2023-07-24 10:09:24 +0200

    ajout message accueil

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'presentation' ,  title:'Présentation', loadComponent: ()=> import('./components/presentation/presentation.component').then(module =>module.PresentationComponent)},
   { path: 'cv' ,  title:'Cv', loadComponent: ()=> import('./components/cv/cv.component').then(module =>module.CvComponent)},
   { path: 'accueil' ,  title:'Accueil', loadComponent: ()=> import('./components/accueil/accueil.component').then(module =>module.AccueilComponent)},
+  { path: 'project' ,  title:'Projet', loadComponent: ()=> import('./components/projects/projects.component').then(module =>module.ProjectsComponent)},
   { path: '' ,  title:'Accueil', loadComponent: ()=> import('./components/accueil/accueil.component').then(module =>module.AccueilComponent)},
 ]
 
